888833da02a6b266b4fb2e7cbf113621e40fadc3
René Löscher <reneloescher@gmx.net>
2017-01-20 21:30:59 +0100

    Created NavMesh and test movement of AI

---
+++ Assets/Scripts/AI/ChildController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.AI
+{
+    public class ChildController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] children;
+
+        [SerializeField]
+        private GameObject[] testWayPoints;
+
+        void Update()
+        {
+            CheckInput();
+        }
+
+        private void CheckInput()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+            {
+                NavMeshAgent agent = children[0].GetComponent<NavMeshAgent>();
+
+                agent.SetDestination(testWayPoints[0].transform.position);
+            }
+        }
+    }
+}
