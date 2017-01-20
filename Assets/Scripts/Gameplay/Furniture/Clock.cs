23ce67bf7f235b521a886448eeb11a9d1cd00067
Robin <r0bzilla@gmx.net>
2017-01-21 00:19:24 +0100

    Added a clock to the scene. Non-functional.

---
+++ Assets/Scripts/Gameplay/Furniture/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Furniture
+{
+	public class Clock : MonoBehaviour
+	{
+		[SerializeField]
+		protected Transform MinutePointer;
+
+		[SerializeField]
+		protected Transform SecondPointer;
+
+		public void SetTimeRatio( float ratio)
+		{
+			
+		}
+	}
+}
+
